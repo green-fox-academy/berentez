7947789352ce4c1a446c241a3393432b82d54937
Zoltan Berente <berentez07b@gmail.com>
2020-12-05 11:58:55 +0100

    Changing Math.() into **

--- week-02/day-02/Variables/variable-mutations.ts
+++ week-02/day-02/Variables/variable-mutations.ts
@@ -58,7 +58,7 @@ let i1: number = 10;
 let i2: number = 3;
 // tell if i1 is higher than i2 squared and smaller than i2 cubed (as a boolean)
 
-console.log(Math.pow(i2, 2) < i1 && i1 < Math.pow(i2, 3));
+console.log((i2**2) < i1 && i1 < (i2**3));
 
 
 
