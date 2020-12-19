8bb5d56e0cae7a1452dcd161f821c306a3b7af34
Zoltan Berente <berentez07b@gmail.com>
2020-12-19 12:02:59 +0100

    added the two function into one

--- week-04/day-03/line-play.ts
+++ week-04/day-03/line-play.ts
@@ -12,41 +12,28 @@ let rate: number = 25;
 
 function drawStroke( x: number, y: number){
     for (let i: number = 0; i < canvas.width / rate ; i ++){
-
+        ctx.strokeStyle = 'purple';
         ctx.beginPath();
         ctx.moveTo(x, 0);
         ctx.lineTo(canvas.width, y);
         ctx.stroke();
 
-        x += rate;
-        y += rate;
-
-}
-
-}
-
-function drawStroke2( x: number, y: number){
-    for (let i: number = 0; i < rate - 1; i ++){
-
+        ctx.strokeStyle = 'green';
         ctx.beginPath();
         ctx.moveTo(0, x);
         ctx.lineTo(y, canvas.height);
         ctx.stroke();
 
+
         x += rate;
         y += rate;
 
 }
 
-    
 }
 
 
-ctx.strokeStyle = 'purple';
 drawStroke( canvas.width / 20, canvas.height / 20 +10);
-
-ctx.strokeStyle = 'green';
-drawStroke2( canvas.width / 20, canvas.height / 20 +10);
 
 // Körítő fehér négyzet
 
