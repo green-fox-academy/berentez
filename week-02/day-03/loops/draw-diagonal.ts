bc1d8071592fa4491815056c65df99d5b0d9ba99
Zoltan Berente <berentez07b@gmail.com>
2020-12-27 15:35:01 +0100

    diagonal

--- week-02/day-03/loops/draw-diagonal.ts
+++ week-02/day-03/loops/draw-diagonal.ts
@@ -1,38 +1,43 @@
+  
 'use strict';
 
 let lineCount: number = 6;
 
-// Write a program that draws a square like this:
+// Write a program that draws a
+// square like this:
 //
 // %%%%%%
-// %    %
-// %    %
-// %    %
-// %    %
+// %%   %
+// % %  %
+// %  % %
+// %   %%
 // %%%%%%
 //
 // The square should have as many lines as lineCount is
+
 
 let character : string = ''; 
 
 for (let i: number = 0; i < lineCount; i++){
     if (i === 0 || i === lineCount - 1){
-        for (let i: number = 0; i < lineCount; i++){
+        for (let n: number = 0; n < lineCount; n++){
             character = character + '%';
         
         }
     } else {
-        for (let i: number = 0; i < lineCount; i++){
-           if (i === 0 || i === lineCount - 1){
+        for (let n: number = 0; n < lineCount; n++){
+           if (n === 0 || n === lineCount - 1){
                 character = character + '%';
                 
+           } else if(n === i){
+                character = character + '%';
+               
+               
            } else {
                character = character + ' '; 
-               
            }
         }
     }
     console.log(character);
     character = '';
 }
-
