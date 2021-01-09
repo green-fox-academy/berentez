16de9ddecc66df8e1a0247df4ad7b4ad35089be1
Zoltan Berente <berentez07b@gmail.com>
2021-01-09 11:16:01 +0100

    deleted unnecessary variables

--- week-03/day-03/old-exercises/festival-entry.ts
+++ week-03/day-03/old-exercises/festival-entry.ts
@@ -6,9 +6,9 @@ let watchlist: string[] = [];
 let securityAlcoholLoot: number = 0;
 
 type FestivalGoer = {
- name: string,
- alcohol: number,
- guns: number
+  name: string,
+  alcohol: number,
+  guns: number
 };
 
 const queue: FestivalGoer[] = [
@@ -30,27 +30,27 @@ const queue: FestivalGoer[] = [
 // If guns are found, remove them and put them on the watchlist (only the names)
 // If alcohol is found confiscate it (set it to zero and add it to securityAlcholLoot) and let them enter the festival
 
-function securityCheck(player: any[]){
+function securityCheck(player: any[]) {
   let output: string[] = [];
   let newArray: any[] = [];
-  for (let i: number = 0; i < player.length; i++){
-        if ( player[i].guns > 0){
-            watchlist.push(player[i].name);
-        } else {
-          output.push(player[i].name);
-          newArray.push(player[i]);
-        }
-  } 
-  let alcoholArray: number[] = [];
-  for (let i: number = 0; i < newArray.length; i++ ){
-      securityAlcoholLoot = securityAlcoholLoot + newArray[i].alcohol;
+  for (let i: number = 0; i < player.length; i++) {
+    if (player[i].guns > 0) {
+      watchlist.push(player[i].name);
+    } else {
+      output.push(player[i].name);
+      newArray.push(player[i]);
+    }
   }
-  return  output;
+
+  for (let i: number = 0; i < newArray.length; i++) {
+    securityAlcoholLoot = securityAlcoholLoot + newArray[i].alcohol;
+  }
+  return output;
 }
 
 console.log('They can enter the festival: ' + securityCheck(queue).join(', '));
 console.log('On watchlist: ' + watchlist.join(', '));
-console.log(securityAlcoholLoot);
+console.log('Alcohol found: ' + securityAlcoholLoot);
 
 
 
