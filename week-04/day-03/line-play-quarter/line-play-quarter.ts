80c6e19eb9d3ab30c3135ee1b1718b00c82e3ebf
Zoltan Berente <berentez07b@gmail.com>
2021-01-09 15:35:04 +0100

    format

--- week-04/day-03/line-play-quarter/line-play-quarter.ts
+++ week-04/day-03/line-play-quarter/line-play-quarter.ts
@@ -16,74 +16,61 @@ let line: number = 0;
 
 //everything in one function
 
-function draw(x: number, y: number, quarterNum: number) {
-    for (let k: number = 0; k <= Math.sqrt(quarter); k++) {                              //k * i = square number
+function draw(x: number, y: number): void {
+	for (let k: number = 0; k <= Math.sqrt(quarter); k++) {                        //k * i = square number
 
-        for (let i: number = 0; i <= Math.sqrt(quarter); i++) {
+		for (let i: number = 0; i <= Math.sqrt(quarter); i++) {
 
-            for (let n: number = 0; n < lineCount; n++) {                              //filling up i.raw 
+			for (let n: number = 0; n < lineCount; n++) {                              //filling up i.raw 
 
-                ctx.strokeStyle = 'purple';
-                ctx.lineWidth = 1;
-                ctx.beginPath();
-                ctx.moveTo(x, line);
-                ctx.lineTo((i + 1) * (canvas.width / Math.sqrt(quarter)), y);
-                ctx.stroke();
+				ctx.strokeStyle = 'purple';
+				ctx.lineWidth = 1;
+				ctx.beginPath();
+				ctx.moveTo(x, line);
+				ctx.lineTo((i + 1) * (canvas.width / Math.sqrt(quarter)), y);
+				ctx.stroke();
 
-                x += rate;
-                y += rate;
+				x += rate;
+				y += rate;
+			}
 
-            }
+			x = rate + i * (canvas.width / Math.sqrt(quarter));
+			y = rate + rate / 2 + k * (canvas.width / Math.sqrt(quarter));
 
-            x = rate + i * (canvas.width / Math.sqrt(quarter));
-            y = rate + rate / 2 + k * (canvas.width / Math.sqrt(quarter));
+			for (let n: number = 0; n < lineCount; n++) {                              // filling up i.column 
 
-            for (let n: number = 0; n < lineCount; n++) {                              // filling up i.column 
+				ctx.strokeStyle = 'green';
+				ctx.beginPath();
+				ctx.moveTo(line, x);
+				ctx.lineTo(y, (i + 1) * (canvas.height / Math.sqrt(quarter)));
+				ctx.stroke();
 
-                ctx.strokeStyle = 'green';
-                ctx.beginPath();
-                ctx.moveTo(line, x);
-                ctx.lineTo(y, (i + 1) * (canvas.height / Math.sqrt(quarter)));
-                ctx.stroke();
+				x += rate;
+				y += rate;
+			}
 
-                x += rate;
-                y += rate;
+			x = i * (canvas.width / Math.sqrt(quarter));
+			x = x + (canvas.width / Math.sqrt(quarter));
+			y = k * (canvas.height / Math.sqrt(quarter)) + (rate + rate / 2);
+		}
 
-            }
+		x = rate
+		y = y + (canvas.width / Math.sqrt(quarter));
+		line = line + (canvas.height / Math.sqrt(quarter));
 
-            x = i * (canvas.width / Math.sqrt(quarter));
-            x = x + (canvas.width / Math.sqrt(quarter));
-            y = k * (canvas.height / Math.sqrt(quarter)) + (rate + rate / 2);
+		ctx.strokeStyle = 'white';                                                       // white borders for the squares
+		ctx.lineWidth = rate / 2;                                                        // k must <= to Math.sqrt(quarter) for a full border.  
+		ctx.beginPath();                                                                 // so it runs one more time just for the white lines.   
+		ctx.moveTo(0, k * canvas.width / Math.sqrt(quarter));
+		ctx.lineTo(canvas.width, k * canvas.width / Math.sqrt(quarter));
+		ctx.stroke();
 
-        }
-
-
-        x = rate
-        y = y + (canvas.width / Math.sqrt(quarter));
-        line = line + (canvas.height / Math.sqrt(quarter));
-
-        ctx.strokeStyle = 'white';                                                       // white borders for the squares
-        ctx.lineWidth = rate / 2;                                                        // k must <= to Math.sqrt(quarter) for a full border.  
-        ctx.beginPath();                                                                 // so it runs one more time just for the white lines.   
-        ctx.moveTo(0, k * canvas.width / Math.sqrt(quarter));
-        ctx.lineTo(canvas.width, k * canvas.width / Math.sqrt(quarter));
-        ctx.stroke();
-
-        ctx.strokeStyle = 'white';
-        ctx.lineWidth = rate / 2;
-        ctx.beginPath();
-        ctx.moveTo(k * (canvas.width / Math.sqrt(quarter)), 0);
-        ctx.lineTo(k * (canvas.width / Math.sqrt(quarter)), canvas.width);
-        ctx.stroke();
-
-
-    }
-
+		ctx.beginPath();
+		ctx.moveTo(k * (canvas.width / Math.sqrt(quarter)), 0);
+		ctx.lineTo(k * (canvas.width / Math.sqrt(quarter)), canvas.width);
+		ctx.stroke();
+	}
 }
 
-
-
-
-
-draw(x, y, quarter);
+draw(x, y);
 
