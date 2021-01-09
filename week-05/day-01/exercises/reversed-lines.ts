73ed5ba2a4b459df43c84889124cb99056ef6b3e
Zoltan Berente <berentez07b@gmail.com>
2021-01-09 13:36:50 +0100

    changed to \r

--- week-05/day-01/exercises/reversed-lines.ts
+++ week-05/day-01/exercises/reversed-lines.ts
@@ -1,14 +1,20 @@
-// Create a method that decrypts reversed-order.txt
+// Create a method that decrypts reversed-lines.txt
 
 import * as fs from 'fs';
 
 function reverseLine(file: string) {
     let readFile = fs.readFileSync(file, 'utf-8');
-    let lineSplit: string[] = readFile.split('\n');
-    let cryptedMessage: string[] = [];
-    for (let i: number = lineSplit.length - 1; i >= 0; i--) {
-        cryptedMessage.push(lineSplit[i]);
+    let lineSplit: string[] = readFile.split('\r');
+    let lineArray: string[] = [];
+    for (let i: number = 0; i < lineSplit.length; i++) {
+        let line: string = lineSplit[i];
+        for (let n: number = line.length - 1; n >= 0; n--) {
+            lineArray.push(line[n])
+        }
+
     }
-    return cryptedMessage.join('\n');
+
+
+    return lineArray.join('')
 }
-console.log(reverseLine('reversed-order.txt'))
+console.log(reverseLine('reversed-lines.txt'))
