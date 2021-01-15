791b3c9801d1326b938769a1d637a70fc5d50b10
Zoltan Berente <berentez07b@gmail.com>
2021-01-15 20:28:45 +0100

    format

--- week-06/day-02/sumdigit.ts
+++ week-06/day-02/sumdigit.ts
@@ -1,21 +1,17 @@
 // Given a non-negative integer n, return the sum of its digits recursively (without loops).
-export {}
+export {};
 
 let sum: number = 0;
-let digit: number = 0
+let digit: number = 0;
 
 function sumOfDigits(n: number): number {
-    if (n.toString().length <= 1){
-        return sum + n;
-    } else {
-        digit = Math.floor(n / 10**(n.toString().length - 1))
-        sum += digit;
-        return sumOfDigits(n - (digit * (10**(n.toString().length - 1))))
-    }
+  if (n.toString().length <= 1) {
+    return sum + n;
+  } else {
+    digit = Math.floor(n / 10 ** (n.toString().length - 1));
+    sum += digit;
+    return sumOfDigits(n - digit * 10 ** (n.toString().length - 1));
+  }
 }
 
-
-
-
-console.log(sumOfDigits(6543218))
-
+console.log(sumOfDigits(6543218));
