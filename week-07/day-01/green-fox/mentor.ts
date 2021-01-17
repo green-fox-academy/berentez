c7fd4e74f5f6b1c04bd82bf6f1e94933435ac08f
Zoltan Berente <berentez07b@gmail.com>
2021-01-17 20:32:08 +0100

    in modules

---
+++ week-07/day-01/green-fox/mentor.ts
@@ -0,0 +1,30 @@
+// Mentor
+// Create a Mentor class that has the same fields and methods as the Person class, and has the following additional
+//// fields:
+//// - level: the level of the mentor (junior / intermediate / senior)
+//// methods:
+//// -  getGoal(): prints out 'My goal is: Educate brilliant junior software developers.'
+//// -  introduce(): 'Hi, I'm name, a age year old gender level mentor.'
+//// The Mentor class has the following constructors:
+//// - Mentor(name, age, gender, level)
+//// - Mentor(): sets name to Jane Doe, age to 30, gender to female, level to intermediate.
+
+import { Person } from './person';
+
+ export class Mentor extends Person {
+    level: string = 'junior' || 'intermediate' || 'senior';
+  
+    constructor(name: string = 'Jane Doe', age: number = 30, gender: string = 'female', level: string = 'intermediate') {
+          super(name, age, gender);
+          this.level = level;
+      }
+  
+    getGoal(): void {
+      console.log('My goal is: Educate brilliant junior software developers.');
+    }
+    introduce(): void {
+      console.log(
+        "Hi, I'm " + this.name + ', a ' + this.age + ' year old ' + this.gender + ' ' + this.level + ' mentor.'
+      );
+    }
+  }
