5164f7835e5bebb81bc8da24f306f7dd2b87ee40
Zoltan Berente <berentez07b@gmail.com>
2021-01-20 17:52:13 +0100

    format

--- week-05/day-01/reading-files/count-lines.ts
+++ week-05/day-01/reading-files/count-lines.ts
@@ -1,5 +1,4 @@
-export{};
-
+export {};
 
 // Write a function that takes a filename as string,
 // then returns the number of lines the file contains.
@@ -8,17 +7,14 @@ export{};
 
 const fs = require('fs');
 
-function countLines(filename: string){
-    const txt = fs.readFileSync(filename, 'utf-8');
-    return txt.split(/\r\n|\r|\n/).length;
-    
+function countLines(filename: string) {
+  const txt = fs.readFileSync(filename, 'utf-8');
+  return txt.split(/\r\n|\r|\n/).length;
 }
-
 
 try {
-    countLines('lyrics.txt')
-    console.log(countLines('lyrics.txt'))
-}
-catch (err) {
-    console.log(0);
+  countLines('lyrics.txt');
+  console.log(countLines('lyrics.txt'));
+} catch (err) {
+  console.log(0);
 }
--- week-05/day-01/reading-files/print-each-line.ts
+++ week-05/day-01/reading-files/print-each-line.ts
@@ -3,18 +3,15 @@
 // If the program is unable to read the file (for example it does not exist),
 // then it should print the following error message: "Unable to read file: my-file.txt"
 
-export{}
+export {};
 
 const fs = require('fs');
-
-
-
 
 // with try - catch - used https://nodejs.dev/learn/reading-files-with-nodejs (This helped me a lot)
 
 try {
-    const data = fs.readFileSync('my-file1.txt', 'utf8');
-    console.log(data);
-} catch (err) { 
-    console.error("Unable to read file: my-file.txt");
+  const data = fs.readFileSync('my-file1.txt', 'utf8');
+  console.log(data);
+} catch (err) {
+  console.error('Unable to read file: my-file.txt');
 }
