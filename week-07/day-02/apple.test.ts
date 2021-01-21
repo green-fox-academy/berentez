8958f517d4a126ebd106d8c90617f6c8b6a0fb0c
Zoltan Berente <berentez07b@gmail.com>
2021-01-21 16:33:36 +0100

    test working

---
+++ week-07/day-02/apple.test.ts
@@ -0,0 +1,22 @@
+// Create a test for that:
+// Create a test method
+// Instantiate an Object from your class in the method
+// Use the t.equal()
+// The expected parameter should be the same string (eg. 'apple')
+
+// The actual parameter should be the return value of the method (eg. myobject.getApple())
+// Run the test
+// Change the expected value to make the test failing
+// Run the test
+// Fix the returned value to make the test succeeding again
+
+import { Apple } from './apple';
+const test = require('tape');
+
+test('return apple', (t) => {
+  const apple = new Apple();
+  const expected = 'apple';
+
+  t.equal(apple.getApple(), expected);
+  t.end();
+});
