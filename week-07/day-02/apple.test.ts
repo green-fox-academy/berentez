eda8a22dc069b8d5c5f53b0427482049d09ca6b1
Zoltan Berente <berentez07b@gmail.com>
2021-01-21 16:34:50 +0100

    working again

--- week-07/day-02/apple.test.ts
+++ week-07/day-02/apple.test.ts
@@ -3,7 +3,6 @@
 // Instantiate an Object from your class in the method
 // Use the t.equal()
 // The expected parameter should be the same string (eg. 'apple')
-
 // The actual parameter should be the return value of the method (eg. myobject.getApple())
 // Run the test
 // Change the expected value to make the test failing
