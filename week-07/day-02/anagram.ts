0976bcda94616f4dbc1b09f200f87d3002e9d58c
Zoltan Berente <berentez07b@gmail.com>
2021-01-22 17:54:55 +0100

    lord Voldemort anagram function

--- week-07/day-02/anagram.ts
+++ week-07/day-02/anagram.ts
@@ -1,12 +1,25 @@
 // Write a function, that takes two strings and returns a boolean value based on if the two strings are Anagramms or not.
 // Create a test for that.
+function spaceDelete(word): string[] {
+  let array: string[] = word.split('');
+  while (array.indexOf(' ') !== -1) {
+    array.splice(array.indexOf(' '), 1);
+  }
+  return array;
+}
 
 function findAnagram(word1: string, word2: string): boolean {
-  if (word1.split('').sort().join('') === word2.split('').sort().join('')) {
+  let array1: string[] = spaceDelete(word1);
+  let array2: string[] = spaceDelete(word2);
+
+  if (array1.sort().join('') === array2.sort().join('')) {
     return true;
   } else {
     return false;
   }
 }
 
-console.log(findAnagram('astronomer', 'moon starer'));
+//Lets imagine the young Voldemort thinking about his super villain name for a minute
+// let word1 = 'tom marvolo riddle';
+// let word2 = 'i am lord voldemort';
+// console.log(findAnagram(word1, word2));
