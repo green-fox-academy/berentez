0d449f9505424e9d9973b003788ee2e995fcf55f
Zoltan Berente <berentez07b@gmail.com>
2021-01-23 13:06:18 +0100

    t.deepEqual()

--- week-07/day-02/anagram-test.ts
+++ week-07/day-02/anagram-test.ts
@@ -1,1 +1,21 @@
-import { findAnagram } from './anagram';
+import { findAnagram, formatWord } from './anagram';
+const test = require('tape');
+
+// Lets imagine young Voldemort thinking about his super villain name for a minute...
+
+test('format string', (t) => {
+  const word: string = 'Tom Denem';
+  const expected: string[] = ['t', 'o', 'm', 'd', 'e', 'n', 'e', 'm'];
+  //thx Livi
+  t.deepEqual(formatWord(word), expected);
+  t.end();
+});
+
+test('Anagram', (t) => {
+  const word1: string = 'Tom Marvolo Riddle';
+  const word2: string = 'I am Lord Voldemort';
+  const expected: boolean = true;
+
+  t.equal(findAnagram(word1, word2), expected);
+  t.end();
+});
--- week-07/day-02/anagram.ts
+++ week-07/day-02/anagram.ts
@@ -1,17 +1,17 @@
 // Write a function, that takes two strings and returns a boolean value based on if the two strings are Anagramms or not.
 // Create a test for that.
 
-function spaceDelete(word): string[] {
-  let array: string[] = word.split('');
+export function formatWord(word: string): string[] {
+  let array: string[] = word.toLowerCase().split('');
   while (array.indexOf(' ') !== -1) {
     array.splice(array.indexOf(' '), 1);
   }
   return array;
 }
 
-function findAnagram(word1: string, word2: string): boolean {
-  let array1: string[] = spaceDelete(word1);
-  let array2: string[] = spaceDelete(word2);
+export function findAnagram(word1: string, word2: string): boolean {
+  let array1: string[] = formatWord(word1);
+  let array2: string[] = formatWord(word2);
 
   if (array1.sort().join('') === array2.sort().join('')) {
     return true;
@@ -19,8 +19,3 @@ function findAnagram(word1: string, word2: string): boolean {
     return false;
   }
 }
-
-//Lets imagine young Voldemort thinking about his super villain name for a minute...
-// let word1 = 'tom marvolo riddle';
-// let word2 = 'i am lord voldemort';
-// console.log(findAnagram(word1, word2));
