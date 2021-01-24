a5a3742cb589b26bdf622f559ebf931379a76efb
Zoltan Berente <berentez07b@gmail.com>
2021-01-24 17:54:50 +0100

    drink(), eat() tested

--- week-07/day-02/animal-test.ts
+++ week-07/day-02/animal-test.ts
@@ -1,11 +1,53 @@
+// Search back in your own project directory the Animal class you made on the OO workshop
+// Create tests for multiple test cases.
+
 import { Animal } from 'D:/greenfox/berentez/week-05/day-03/constructor-usage/animal';
 const test = require('tape');
 
+//Constructor test
 test('testing default hunger:', (t: any) => {
   const dog = new Animal();
   const actual = dog.hunger;
   const expected = 50;
 
+  t.equal(actual, expected);
+  t.end();
+});
+
+//eat() test
+test('testing eat() method: ', (t: any) => {
+  //arrange
+  const dog = new Animal();
+  dog.eat(10);
+  const actual = dog.hunger;
+  const expected = 40;
+
+  t.equal(actual, expected);
+  t.end();
+});
+
+//Overfeeding
+test('testing eat() method: ', (t: any) => {
+  //arrange
+  const dog = new Animal();
+  dog.eat(60);
+  const actual = dog.hunger;
+  const expected = 0;
+
+  //assert
+  t.equal(actual, expected);
+  t.end();
+});
+
+//drink
+test('testing drink():', (t: any) => {
+  //arrange
+  const dog = new Animal();
+  dog.drink(35);
+  const actual = dog.thirst;
+  const expected = 15;
+
+  //assert
   t.equal(actual, expected);
   t.end();
 });
