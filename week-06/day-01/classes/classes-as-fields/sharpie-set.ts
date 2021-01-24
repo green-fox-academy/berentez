65848d06cefa8132b754692d51b8c96b202e4a24
Zoltan Berente <berentez07b@gmail.com>
2021-01-24 12:15:57 +0100

    modified for testing exercises

--- week-06/day-01/classes/classes-as-fields/sharpie-set.ts
+++ week-06/day-01/classes/classes-as-fields/sharpie-set.ts
@@ -6,70 +6,71 @@
 
 ///////////////////////////////////////////////////////////////
 
-class Sharpie {
+export class Sharpie {
   color: string;
   width: number;
-  public inkAmount: number = 100;
+  public inkAmount: number;
 
   constructor(color: string, width: number) {
     this.color = color;
     this.width = width;
+    this.inkAmount = 100;
   }
 
-  use(number: number): void {
-		for (let i: number = 0; i < number; i++){
-			this.inkAmount--;
-		}
+  use(number: number): number {
+    for (let i: number = 0; i < number; i++) {
+      this.inkAmount--;
+    }
+    return this.inkAmount;
   }
 }
 
-class SharpieSet {
-	sharpie: Sharpie[] = [];
+export class SharpieSet {
+  sharpie: Sharpie[] = [];
 
-	constructor(...sharpie: Sharpie[]){
-		this.sharpie.push(...sharpie);
-	}
+  constructor(...sharpie: Sharpie[]) {
+    this.sharpie.push(...sharpie);
+  }
 
-	countUsable(): number {
-		let counter = 0;
-		for (let i: number = 0; i < this.sharpie.length; i++){
-			if (this.sharpie[i].inkAmount > 0){
-				counter++;
-			}
-		}
-		return counter;
-	}
+  countUsable(): number {
+    let counter = 0;
+    for (let i: number = 0; i < this.sharpie.length; i++) {
+      if (this.sharpie[i].inkAmount > 0) {
+        counter++;
+      }
+    }
+    return counter;
+  }
 
-	removeTrash(): void {
-		for (let i: number = 0; i < this.sharpie.length; i++){
-			if (this.sharpie[i].inkAmount === 0){
-				this.sharpie.splice(i, 1);
-			}
-		}
-	}
+  removeTrash(): void {
+    for (let i: number = 0; i < this.sharpie.length; i++) {
+      if (this.sharpie[i].inkAmount === 0) {
+        this.sharpie.splice(i, 1);
+      }
+    }
+  }
 }
-
 
 ///////////////////////////////////////////////////////////////////////////////
 
 //Test
 //Sharpies
-let red = new Sharpie('red', 5);
-let green = new Sharpie('green', 5);
-let blue = new Sharpie('blue', 5);
-let yellow = new Sharpie('yellow', 5);
-let black = new Sharpie('black', 5);
+// let red = new Sharpie('red', 5);
+// let green = new Sharpie('green', 5);
+// let blue = new Sharpie('blue', 5);
+// let yellow = new Sharpie('yellow', 5);
+// let black = new Sharpie('black', 5);
 
 //Sharpieset
-let set = new SharpieSet(red, green, blue, yellow, black);
+// let set = new SharpieSet(red, green, blue, yellow, black);
 
 //using sharpies
-blue.use(72)
-red.use(19);
-black.use(100);
-yellow.use(4)
+// blue.use(72);
+// red.use(19);
+// black.use(100);
+// yellow.use(4);
 
 //sharpieset methods
-console.log(set.countUsable());
-set.removeTrash();
-console.log(set);
+// console.log(set.countUsable());
+// set.removeTrash();
+// console.log(set);
