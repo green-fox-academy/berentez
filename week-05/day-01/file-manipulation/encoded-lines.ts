9b736dc78375987258675d7f349f4910b62b9c5a
Zoltan Berente <berentez07b@gmail.com>
2021-02-09 18:06:52 +0100

    String.fromCharCode()

--- week-05/day-01/file-manipulation/encoded-lines.ts
+++ week-05/day-01/file-manipulation/encoded-lines.ts
@@ -1,44 +1,40 @@
 // Create a method that decrypts encoded-lines.txt
 
-import { indexOf } from '../../../week-03/day-03/strings/simple-replace';
-
 export {};
+import * as fs from 'fs';
+let text: string;
 
-const fs = require('fs');
-let text = fs.readFileSync('encoded-lines.txt', 'utf-8');
+// function encode() {}
 
-function encode() {}
 function splitText(text: string): string[] {
   let characters: string[] = [];
   characters = text.split('');
   return characters;
 }
 
-let list: string[] = splitText(text);
-let charList: number[] = [];
-for (let i: number = 0; i < text.length; i++) {
-  charList.push(text.charCodeAt(i));
+function decode(text: string) {
+  try {
+    text = fs.readFileSync(text, 'utf-8');
+  } catch (err) {
+    console.log(err);
+  }
+  splitText(text);
+  let list: string[] = splitText(text);
+  let charList: number[] = [];
+  for (let i: number = 0; i < text.length; i++) {
+    charList.push(text.charCodeAt(i));
+  }
+
+  for (let i: number = 0; i < charList.length; i++) {
+    if (charList[i] !== 32 && charList[i] !== 13 && charList[i] !== 10) {
+      charList[i] -= 1;
+    }
+  }
+  let decodedArray: string[] = [];
+  for (let i: number = 0; i < charList.length; i++) {
+    decodedArray.push(String.fromCharCode(charList[i]));
+  }
+  return decodedArray.join('');
 }
 
-console.log(charList);
-
-console.log(
-  charList.map((value) => {
-    value += 1;
-  })
-);
-
-// function makeCharacterList(): string[] {
-//   let characterList: string[] = [];
-//   for (let i: number = 0; i < list.length; i++) {
-//     if (characterList.indexOf(text[i]) === -1) {
-//       characterList.push(text[i]);
-//     }
-//   }
-//   return characterList.sort();
-// }
-
-// let sortedList: string[] = makeCharacterList();
-// console.log(sortedList);
-
-// list[i].toString(65535);
+console.log(decode('encoded-lines.txt'));
