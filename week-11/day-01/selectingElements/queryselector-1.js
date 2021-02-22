9ab8d18b3587ed5cccac60b8ea0d3a33d50ac4f6
Zoltan Berente <berentez07b@gmail.com>
2021-02-22 18:50:46 +0100

    finished 3 and reworked 2 with querySelector

--- week-11/day-01/selectingElements/queryselector-1.js
+++ week-11/day-01/selectingElements/queryselector-1.js
@@ -4,18 +4,14 @@ const king = document.querySelector('#b325');
 console.log('king:', king);
 
 //2. 
-const businessman = document.querySelector('p');
-//didn't work with querySelectorAll()
-const lamplighter = document.getElementsByClassName("asteroid b329 big");
-businessman.setAttribute("class", 'big asteroid');
-lamplighter[0].setAttribute('id', 'b329');
-
-//making the array
-const businessLamp = [];
-businessLamp.push(lamplighter[0]);
-businessLamp.push(businessman);
-//printing
+const businessLamp = document.querySelectorAll('.asteroid.big')
 for (let i = 0; i < businessLamp.length; i++){
     console.log(businessLamp[i]);
 }
 
+//3.
+const container = document.querySelector('.container');
+const conceitedKing = container.querySelectorAll('.asteroid');
+for (let i = 0; i < conceitedKing.length; i++){
+    console.log(conceitedKing[i]);
+}
