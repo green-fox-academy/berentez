6126781cf3b79061a5bebf50151f08bbf0c5004f
Zoltan Berente <berentez07b@gmail.com>
2021-02-25 16:45:05 +0100

    on click star added

--- week-11/day-01/manipulating-elements/stars-and-moon.html/script.js
+++ week-11/day-01/manipulating-elements/stars-and-moon.html/script.js
@@ -1,4 +1,4 @@
-// At the beginning, add (append) ten stars to random coordinates via JavaScript. 
+// 1) At the beginning, add (append) ten stars to random coordinates via JavaScript. 
 // You'll have to create an element with class 'star' and set it an inline style. 
 // The left and top coordinates should be in % or vw/vh units so that the stars move if the window size changes.
 
@@ -9,11 +9,24 @@ for (let i = 0; i < 10; i++){
   const newStar = document.createElement('div');
   newStar.classList.add('star');
   newStar.setAttribute('style', `left:${randomNum()}%; top: ${randomNum()}%`)
-  section.appendChild(newStar)
+  section.appendChild(newStar);
 }
-
 
 //for random coordianates
 function randomNum(){
  return Math.floor(Math.random() * 100);
 }
+
+// 2) On click anywhere on section.world, add a star beneath the cursor.
+document.addEventListener('click', addStar);
+
+function addStar(e){
+  const newStar = document.createElement('div');
+  newStar.classList.add('star');
+  // I had to use pixel for this to work
+  newStar.setAttribute('style', `left:${e.clientX}px; top: ${e.clientY}px`)
+  section.appendChild(newStar);
+}
+
+
+
