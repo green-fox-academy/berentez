5cfa4e4a4f25e18907b95361c1bd7a08d3916c53
Zoltan Berente <berentez07b@gmail.com>
2021-02-28 17:55:23 +0100

    simple svg task solved

---
+++ week-11/day-01/svg/task/svg1.ts
@@ -0,0 +1,5 @@
+const rectangle: HTMLElement = document.querySelector('#very-rectangle');
+
+rectangle.setAttribute('x', '50');
+rectangle.setAttribute('y', '50');
+rectangle.setAttribute('style', 'stroke:#ff0000; stroke-width: 10; fill: tomato');
