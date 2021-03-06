a30aa9458cd2d7a9a75f58883c0c2aa4f597327d
Zoltan Berente <berentez07b@gmail.com>
2021-03-06 16:57:50 +0100

    refactoring

--- week-11/day-03/script.ts
+++ week-11/day-03/script.ts
@@ -4,45 +4,43 @@ const title = document.querySelector('.discription h2');
 const text = document.querySelector('.discription p');
 const activePicture: HTMLElement = document.querySelector('#picture img');
 const activeThumbnail: HTMLElement = document.querySelector('.activethumbnail');
-
-//needs refactoring
-
-rightArraw.onclick = () => {
-  const activeThumbnail: HTMLElement = document.querySelector('.activethumbnail');
-  for (let i: number = 0; i < thumbnails.length; i++) {
-    if (thumbnails[i].getAttribute('src') === activeThumbnail.getAttribute('src')) {
-      activePicture.setAttribute('src', thumbnails[i + 1].getAttribute('src'));
-      activeThumbnail.setAttribute('class', '');
-      thumbnails[i + 1].setAttribute('class', 'activethumbnail');
-      title.textContent = thumbnails[i + 1].getAttribute('title');
-      text.textContent = thumbnails[i + 1].getAttribute('text');
-    }
-  }
-};
-
 const leftArraw: HTMLElement = document.querySelector('#navigation.left');
 
+//////////////////////////////////
+
+//arriw right
+rightArraw.onclick = () => {
+  changeByArrow(1);
+};
+
+//arrow left
 leftArraw.onclick = () => {
+  changeByArrow(-1);
+};
+
+//thumbnail click
+for (let i: number = 0; i < thumbnails.length; i++) {
+  thumbnails[i].addEventListener('click', function () {
+    changePic(i, 0);
+  });
+}
+
+//////////////////////////////////
+
+function changeByArrow(direction: number) {
   const activeThumbnail: HTMLElement = document.querySelector('.activethumbnail');
   for (let i: number = 0; i < thumbnails.length; i++) {
     if (thumbnails[i].getAttribute('src') === activeThumbnail.getAttribute('src')) {
-      activePicture.setAttribute('src', thumbnails[i + -1].getAttribute('src'));
-      activeThumbnail.setAttribute('class', '');
-      thumbnails[i - 1].setAttribute('class', 'activethumbnail');
-      title.textContent = thumbnails[i + -1].getAttribute('title');
-      text.textContent = thumbnails[i + -1].getAttribute('text');
+      changePic(i, direction);
     }
   }
-};
+}
 
-// clicking on thumbnails
-for (let i: number = 0; i < thumbnails.length; i++) {
-  thumbnails[i].addEventListener('click', function () {
-    const activeThumbnail: HTMLElement = document.querySelector('.activethumbnail');
-    activeThumbnail.setAttribute('class', '');
-    activePicture.setAttribute('src', thumbnails[i].getAttribute('src'));
-    thumbnails[i].setAttribute('class', 'activethumbnail');
-    title.textContent = thumbnails[i].getAttribute('title');
-    text.textContent = thumbnails[i].getAttribute('text');
-  });
+function changePic(i: number, direction?: number) {
+  const activeThumbnail: HTMLElement = document.querySelector('.activethumbnail');
+  activePicture.setAttribute('src', thumbnails[i + direction].getAttribute('src'));
+  activeThumbnail.setAttribute('class', '');
+  thumbnails[i + direction].setAttribute('class', 'activethumbnail');
+  title.textContent = thumbnails[i + direction].getAttribute('title');
+  text.textContent = thumbnails[i + direction].getAttribute('text');
 }
