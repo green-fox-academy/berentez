95d66bf1ce6629c91018ae517c4bb8c21786044d
Zoltan Berente <berentez07b@gmail.com>
2021-03-09 21:16:54 +0100

    doubling on progress

--- week-13/day-01/app.js
+++ week-13/day-01/app.js
@@ -1,11 +1,25 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 const path = require('path');
+
+const urlencodedParses = bodyParser.urlencoded({extended: false})
 
 app.use(express.static('assets'));
 
 app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+
+//Doubling
+app.get('/doubling', function(req, res){
+  res.render('doubling', {qs: req.query});
+});
+
+app.post('/doubling',  urlencodedParses, function(req, res){
+  console.log(req.body);
+  res.render('doubling', {qs: req.query});
 });
 
 app.listen(3000);
