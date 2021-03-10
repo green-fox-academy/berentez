edd0b364a5b97c7b80c8c62e6893fa1ca5ada822
Zoltan Berente <berentez07b@gmail.com>
2021-03-10 20:44:59 +0100

    greeter on progress

--- week-13/day-01/app.js
+++ week-13/day-01/app.js
@@ -1,3 +1,4 @@
+const { application } = require('express');
 const express = require('express');
 const app = express();
 const path = require('path');
@@ -16,12 +17,45 @@ app.get('/doubling', function(req, res){
   let result = {}; 
 
   if (number === undefined){
-    result = {"error": "Please provide an input!"}
+    result = {'error': 'Please provide an input!'}
   } else {
-      result = {"received": number * 1, "result": number * 2}
+    // had to multiply by one couse it becomes string.
+      result = {'received': number * 1, 'result': number * 2}
   }
   res.end(JSON.stringify(result))
 });
 
+
+
+//Greeter
+
+app.get('/greeter', function (req, res){
+  const urlParams = window.location.search.substr(1);
+  // const name = urlParams.get('name')
+  // const title = urlParams.get('title')
+  const name = req.params[0];
+  const title = req.params[1];
+  let result = {};
+
+  if (name === undefined && title === undefined){
+    result = {'error': 'Please provide a name and a title!'};
+    res.status(400);
+  } else if ( name === undefined){
+      result = {'error': 'Please provide a title!'};
+      res.status(400);
+  } else if (title === undefined){
+      result = {'error': 'Please provide a title!'};
+      res.status(400);
+  } else {
+      result = {'welcome_message': `Oh, hi there ${name}, my dear ${title}!`}
+      res.status(200);
+  }
+  console.log(name)
+  console.log(title)
+
+  res.end(JSON.stringify(result));
+
+});
+ 
 
 app.listen(3000);
