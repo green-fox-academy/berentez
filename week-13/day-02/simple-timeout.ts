69f8fabbb11b5ff3c6d494ac70a381385dfc0b11
Zoltan Berente <berentez07b@gmail.com>
2021-03-13 14:09:30 +0100

    apple printed after 3 sec

---
+++ week-13/day-02/simple-timeout.ts
@@ -0,0 +1,13 @@
+// Write a program that prints apple after 3 seconds.
+
+setTimeout(() => {
+  console.log('lemon');
+}, 1000);
+
+setTimeout(() => {
+  console.log('orange');
+}, 2000);
+
+setTimeout(() => {
+  console.log('apple');
+}, 3000);
