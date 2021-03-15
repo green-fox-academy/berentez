d4811bb556574252256de43dda6748595e07b740
Zoltan Berente <berentez07b@gmail.com>
2021-03-15 11:34:46 +0100

    search by button, printing jso to console

--- week-13/day-03/starwars/app.js
+++ week-13/day-03/starwars/app.js
@@ -1,6 +1,20 @@
+// The input text's content should be sent to the people's api and you should perform a search with it.
+
 const btn = document.querySelector('#btn');
 const search = document.querySelector('#input');
 
 btn.addEventListener('click', function () {
-  console.log(search.value);
+  const http = new XMLHttpRequest();
+  http.open('GET', `https://swapi.dev/api/people/?search=${search.value}`);
+  http.onload = function () {
+    if (http.status >= 200 && http.status < 400) {
+      const myData = JSON.parse(http.responseText);
+      console.log(myData);
+    } else {
+      console.log('We connected to the server, but it returned an error');
+    }
+  };
+  http.send();
 });
+
+console.log(search.value);
