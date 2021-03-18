327f0e24341ecbf064d64031ce42ac4f1bfce92d
Zoltan Berente <berentez07b@gmail.com>
2021-03-18 21:08:14 +0100

    table displayed by complex query

--- week-14/day-01/bookstore/app.js
+++ week-14/day-01/bookstore/app.js
@@ -25,7 +25,7 @@ connection.connect((err, result) => {
 app.use('/', express.static('assets'));
 app.use(express.json());
 
-app.get('/book', (req, res) => {
+app.get('/booktitle', (req, res) => {
   //book_name query
   connection.query('SELECT book_name FROM book_mast;', (err, rows) => {
     if (err) {
@@ -37,6 +37,23 @@ app.get('/book', (req, res) => {
 
     res.json(rows);
   });
+});
+
+app.get('/book', (req, res) => {
+  connection.query(
+    'SELECT book_name, aut_name, cate_descrip, pub_name, book_price FROM book_mast JOIN author ON book_mast.aut_id = author.aut_id JOIN category ON book_mast.cate_id = category.cate_id JOIN publisher ON book_mast.pub_id = publisher.pub_id',
+    (err, rows) => {
+      if (err) {
+        res.status(500).json({
+          error: err.message,
+        });
+        return null;
+      }
+
+      res.json(rows);
+      console.log(rows);
+    }
+  );
 });
 
 process.on('uncaughtException', (err) => {
--- week-14/day-01/bookstore/assets/script.js
+++ week-14/day-01/bookstore/assets/script.js
@@ -1,27 +1,66 @@
 const apiURL = 'http://localhost:3000';
 
-function getBooks() {
+// function getBooks() {
+//   const httpRequest = new XMLHttpRequest();
+//   httpRequest.onload = () => {
+//     const books = JSON.parse(httpRequest.response);
+//     addBooks(books);
+//   };
+//   httpRequest.open('GET', `${apiURL}/booktitle`, true);
+//   httpRequest.send();
+// }
+
+function getBookData() {
   const httpRequest = new XMLHttpRequest();
   httpRequest.onload = () => {
     const books = JSON.parse(httpRequest.response);
-    addBooks(books);
+    const tBody = document.querySelector('tbody');
+
+    addAllData(books, tBody);
   };
   httpRequest.open('GET', `${apiURL}/book`, true);
   httpRequest.send();
 }
 
 window.onload = () => {
-  getBooks();
+  getBookData();
 };
 
-//adding books
-function addBooks(books) {
-  const tBody = document.querySelector('tbody');
-  books.forEach((bookName) => {
+function addAllData(books, body) {
+  books.forEach((book) => {
     const tr = document.createElement('tr');
-    const tdTitle = document.createElement('td');
-    tdTitle.textContent = bookName.book_name;
-    tr.appendChild(tdTitle);
-    tBody.appendChild(tr);
+
+    const title = document.createElement('td');
+    title.textContent = book.book_name;
+    tr.appendChild(title);
+
+    const author = document.createElement('td');
+    author.textContent = book.aut_name;
+    tr.appendChild(author);
+
+    const category = document.createElement('td');
+    category.textContent = book.cate_descrip;
+    tr.appendChild(category);
+
+    const publisher = document.createElement('td');
+    publisher.textContent = book.pub_name;
+    tr.appendChild(publisher);
+
+    const price = document.createElement('td');
+    price.textContent = `${book.book_price} USD`;
+    tr.appendChild(price);
+    body.appendChild(tr);
   });
 }
+
+//adding books
+// function addBooks(books) {
+//   const tBody = document.querySelector('tbody');
+//   books.forEach((bookName) => {
+//     const tr = document.createElement('tr');
+//     const title = document.createElement('td');
+//     title.textContent = bookName.book_name;
+//     tr.appendChild(title);
+//     tBody.appendChild(tr);
+//   });
+// }
