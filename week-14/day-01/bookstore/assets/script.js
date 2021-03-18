bde4dc7cb3b521344c9018b1ce1aef987052a705
Zoltan Berente <berentez07b@gmail.com>
2021-03-18 19:05:46 +0100

    Titles listed in the first column

--- week-14/day-01/bookstore/assets/script.js
+++ week-14/day-01/bookstore/assets/script.js
@@ -3,8 +3,8 @@ const apiURL = 'http://localhost:3000';
 function getBooks() {
   const httpRequest = new XMLHttpRequest();
   httpRequest.onload = () => {
-    const books = httpRequest.response;
-    console.log(books);
+    const books = JSON.parse(httpRequest.response);
+    addBooks(books);
   };
   httpRequest.open('GET', `${apiURL}/book`, true);
   httpRequest.send();
@@ -13,3 +13,15 @@ function getBooks() {
 window.onload = () => {
   getBooks();
 };
+
+//adding books
+function addBooks(books) {
+  const tBody = document.querySelector('tbody');
+  books.forEach((bookName) => {
+    const tr = document.createElement('tr');
+    const tdTitle = document.createElement('td');
+    tdTitle.textContent = bookName.book_name;
+    tr.appendChild(tdTitle);
+    tBody.appendChild(tr);
+  });
+}
