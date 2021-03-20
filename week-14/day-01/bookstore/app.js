630f12208f554983f92daee08fcfa056188b0f2f
Zoltan Berente <berentez07b@gmail.com>
2021-03-20 15:16:27 +0100

    filters working at /book url

--- week-14/day-01/bookstore/app.js
+++ week-14/day-01/bookstore/app.js
@@ -19,7 +19,7 @@ connection.connect((err, result) => {
     console.log(err.message);
     return;
   }
-  console.log('\x1b[32m', 'Database connection success');
+  console.log('\u001b[36m', 'Database connection success');
 });
 
 app.use('/', express.static('assets'));
@@ -40,9 +40,15 @@ app.get('/booktitle', (req, res) => {
 });
 
 app.get('/book', (req, res) => {
-  connection.query(
-    'SELECT book_name, aut_name, cate_descrip, pub_name, book_price FROM book_mast JOIN author ON book_mast.aut_id = author.aut_id JOIN category ON book_mast.cate_id = category.cate_id JOIN publisher ON book_mast.pub_id = publisher.pub_id',
-    (err, rows) => {
+  const sqlString =
+    'SELECT book_name, aut_name, cate_descrip, pub_name, book_price FROM book_mast JOIN author ON book_mast.aut_id = author.aut_id JOIN category ON book_mast.cate_id = category.cate_id JOIN publisher ON book_mast.pub_id = publisher.pub_id';
+
+  const query = req.query;
+  let where = '';
+
+  //If there's no query
+  if (Object.keys(query).length === 0) {
+    connection.query(sqlString, (err, rows) => {
       if (err) {
         res.status(500).json({
           error: err.message,
@@ -51,9 +57,44 @@ app.get('/book', (req, res) => {
       }
 
       res.json(rows);
-      console.log(rows);
+      return;
+    });
+  } else {
+    where += ' WHERE ';
+    console.log(Object.keys(query).length);
+    for (let i = 0; i < Object.keys(query).length; i++) {
+      console.log('YO');
+      let queryElement = Object.keys(query)[i];
+      console.log(Object.values(query)[i]);
+      if (queryElement === 'category') {
+        where += `cate_descrip LIKE '${Object.values(query)[i]}'`;
+      } else if (queryElement === 'publisher') {
+        where += `pub_name LIKE '${Object.values(query)[i].split(' ').join('')}'`;
+      } else if (queryElement === 'plt') {
+        where += `book_price < '${parseInt(Object.values(query)[i])}'`;
+      } else if (queryElement === 'pgt') {
+        where += `book_price > '${parseInt(Object.values(query)[i])}'`;
+      }
+
+      if (i !== Object.keys(query).length - 1) {
+        where += ' AND ';
+      } else {
+        where += ';';
+      }
     }
-  );
+    console.log(sqlString + where);
+    connection.query(sqlString + where, (err, rows) => {
+      if (err) {
+        res.status(500).json({
+          error: err.message,
+        });
+        return null;
+      }
+      console.log(rows);
+
+      res.json(rows);
+    });
+  }
 });
 
 process.on('uncaughtException', (err) => {
--- week-14/day-01/bookstore/assets/script.js
+++ week-14/day-01/bookstore/assets/script.js
@@ -1,14 +1,32 @@
 const apiURL = 'http://localhost:3000';
 
-// function getBooks() {
-//   const httpRequest = new XMLHttpRequest();
-//   httpRequest.onload = () => {
-//     const books = JSON.parse(httpRequest.response);
-//     addBooks(books);
-//   };
-//   httpRequest.open('GET', `${apiURL}/booktitle`, true);
-//   httpRequest.send();
-// }
+//1st task
+function getBooks() {
+  const httpRequest = new XMLHttpRequest();
+  httpRequest.onload = () => {
+    const books = JSON.parse(httpRequest.response);
+    addBooks(books);
+  };
+  httpRequest.open('GET', `${apiURL}/booktitle`, true);
+  httpRequest.send();
+}
+
+// adding books
+function addBooks(books) {
+  const tBody = document.querySelector('tbody');
+  books.forEach((bookName) => {
+    const tr = document.createElement('tr');
+    const title = document.createElement('td');
+    title.textContent = bookName.book_name;
+    tr.appendChild(title);
+    tBody.appendChild(tr);
+  });
+}
+
+//2nd task
+// All books with full data
+// List the following data: - title of book - name of author - category - name of publisher - price
+// Return an HTML file with a <table> that contains these information
 
 function getBookData() {
   const httpRequest = new XMLHttpRequest();
@@ -21,10 +39,6 @@ function getBookData() {
   httpRequest.open('GET', `${apiURL}/book`, true);
   httpRequest.send();
 }
-
-window.onload = () => {
-  getBookData();
-};
 
 function addAllData(books, body) {
   books.forEach((book) => {
@@ -47,20 +61,18 @@ function addAllData(books, body) {
     tr.appendChild(publisher);
 
     const price = document.createElement('td');
-    price.textContent = `${book.book_price} USD`;
+    price.textContent = `${book.book_price} $`;
     tr.appendChild(price);
     body.appendChild(tr);
   });
 }
 
-//adding books
-// function addBooks(books) {
-//   const tBody = document.querySelector('tbody');
-//   books.forEach((bookName) => {
-//     const tr = document.createElement('tr');
-//     const title = document.createElement('td');
-//     title.textContent = bookName.book_name;
-//     tr.appendChild(title);
-//     tBody.appendChild(tr);
-//   });
-// }
+window.onload = () => {
+  getBookData();
+};
+// Create filters
+// Add filtering options to the previous endpoint
+// If a filter exists, only list records in case the cell matches the filter's value
+// Implement these filters as query string parameters: - Category as category - Publisher as publisher - Price lower than as plt - Price greater than as pgt
+// Multiple filters can be present at the same time
+// For example /books?category=Science should only return books in the Science category.
