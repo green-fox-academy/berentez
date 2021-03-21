e34c34ab021c321ac8d3cdc015f8c3556d269742
Zoltan Berente <berentez07b@gmail.com>
2021-03-21 11:01:39 +0100

    data read

--- week-14/day-01/import/script.ts
+++ week-14/day-01/import/script.ts
@@ -1,5 +1,8 @@
-const csvtojson = require('csvtojson');
+export {};
+const fs = require('fs');
 const mysql = require('mysql');
+
+const csv = 'users.csv';
 
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -13,53 +16,35 @@ connection.connect((err, result) => {
     console.log(err.message);
     return;
   }
-  console.log('\u001b[36m', 'Database connection success');
-  connection.query(
-    `CREATE TABLE bookinfo.userdata (
-    id INT NOT NULL AUTO_INCREMENT,
-    prefix VARCHAR(40) NULL,
-    first_name VARCHAR(45) NOT NULL,
-    last_name VARCHAR(45) NOT NULL,
-    address VARCHAR(100) NULL,
-    height FLOAT NULL,
-    bitcoin_address VARCHAR(34) NULL,
-    new_tablecol VARCHAR(45) NULL,
-    color_preference VARCHAR(7) NULL,
-    PRIMARY KEY (id),
-    UNIQUE INDEX id_UNIQUE (id ASC) VISIBLE);`,
-    (err) => {
-      if (err) {
-        console.log('ERROR: ', err);
-      }
-    }
-  );
+  console.log('Database connection success');
 });
 
-const csv = 'users.csv';
-
-csvtojson()
-  .fromFile(csv)
-  .then((source) => {
-    for (let i = 0; i < source.length; i++) {
-      let id = source[i]['id'],
-        prefix = source[i]['prefix'],
-        first_name = source[i]['first_name'],
-        last_name = source[i]['last_name'],
-        address = source[i]['address'],
-        height = source[i]['height'],
-        bitcoin_address = source[i]['bitcoin_address'],
-        color_preference = source[i]['color_preference'];
-
-      let insertStatement: string = `INSERT INTO userdata values(?, ?, ?, ?, ?, ? ,? ,?)`;
-
-      const items: string[] = [id, prefix, first_name, last_name, address, height, bitcoin_address, color_preference];
-
-      connection.query(insertStatement, items, (err, results, fields) => {
-        if (err) {
-          console.log('Unable to insert item at row ', i + 1);
-          return console.log(err);
-        }
-      });
-    }
-    console.log('All items stored into database successfully');
+function readData(): string {
+  const data: string = fs.readFileSync('./users.csv', 'utf8', (err, data) => {
+    if (err) throw err;
   });
+  return data;
+}
+
+function splitData(text) {
+  const splittedData = text.split(/\r\n|\r|\n|\,/);
+  return splittedData;
+}
+
+function dataManipulation() {
+  const data = readData();
+
+  return splitData(data);
+}
+
+// connection.query(
+//   'INSERT INTO userdata (id, prefix, first_name, last_name, address, height, bitcoin_address, color_preference) VALUES ',
+//   dataManipulation(),
+//   (err, result) => {
+//     if (err) {
+//       console.log(err);
+//       return;
+//     }
+//     result.json();
+//   }
+// );
