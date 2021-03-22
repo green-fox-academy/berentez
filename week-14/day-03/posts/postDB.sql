b4d9e96a3ba97bd68ec54f6aea7ead04da473dca
Zoltan Berente <berentez07b@gmail.com>
2021-03-22 20:41:56 +0100

    C:/Program Files/Git/post endpoint get request

---
+++ week-14/day-03/posts/postDB.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE reddit CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE reddit;
+CREATE TABLE `reddit`.`post` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(100) NOT NULL,
+  `url` VARCHAR(100) NOT NULL,
+  `timestamp` TIMESTAMP NULL,
+  `score` INT NULL,
+  `owner` VARCHAR(45) NULL,
+  `vote` INT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE
+);
+INSERT INTO
+  `reddit`.`post`
+VALUES
+  (
+    '25',
+    'Dear JavaScript',
+    'http://9gag.com',
+    '1494339525',
+    '791',
+    'null',
+    '1'
+  )
--- week-14/day-03/server.js
+++ week-14/day-03/server.js
@@ -6,8 +6,36 @@ const PORT = 3005;
 
 app.use(express.json());
 
+const conn = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'fZn4g',
+  database: 'reddit',
+});
+
+conn.connect((err) => {
+  if (err) {
+    console.log(err.toString());
+    return;
+  }
+  console.log('connection established to database');
+});
+
 app.get('/hello', (req, res) => {
   res.send('hello world');
+});
+
+app.get('/posts', (req, res) => {
+  conn.query('SELECT * FROM post', (err, results) => {
+    if (err) {
+      res.status(500).json({
+        error: err.message,
+      });
+      return;
+    }
+
+    res.status(200).json(results);
+  });
 });
 
 app.listen(PORT, () => {
