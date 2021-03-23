99382c7dee9c117c3c182b3ae861bb64347b30e9
Zoltan Berente <berentez07b@gmail.com>
2021-03-23 17:25:28 +0100

    second way is with a counter var

--- week-15/clickOnce/script.ts
+++ week-15/clickOnce/script.ts
@@ -9,3 +9,15 @@ btn.addEventListener('click', function () {
   bd.appendChild(newTimestamp);
   btn.disabled = true;
 });
+
+//Second way to solve this task
+let click: number = 0;
+
+bd.addEventListener('click', function () {
+  if (click < 1) {
+    const newTimestamp: HTMLElement = document.createElement('p');
+    newTimestamp.innerHTML = timestamp.toString();
+    bd.appendChild(newTimestamp);
+    click++;
+  }
+});
