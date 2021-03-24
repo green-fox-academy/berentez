c3600c655d11ac0a8e7cc35e9b8c363269ff948d
Zoltan Berente <berentez07b@gmail.com>
2021-03-24 15:11:04 +0100

    added ts file

--- week-15/day-01/delayed/script.ts
+++ week-15/day-01/delayed/script.ts
@@ -1,1 +1,12 @@
 // Create a simple HTML document with one button. If the user clicks the button it should wait 2 seconds and it should show a text under the button: 2 seconds ellapsed
+
+const buton: HTMLButtonElement = document.querySelector('button');
+const textBox: HTMLDivElement = document.querySelector('#display');
+
+buton.addEventListener('click', function (e) {
+  const text = document.createElement('h1');
+  text.innerHTML = '2 seconds ellapsed Dude!';
+  setTimeout(() => {
+    textBox.appendChild(text);
+  }, 2000);
+});
