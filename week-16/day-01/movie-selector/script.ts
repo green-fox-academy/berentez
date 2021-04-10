16fe9061ea452c87f603518020e639a6b63d3846
Zoltan Berente <berentez07b@gmail.com>
2021-04-10 15:17:42 +0200

    Tomi helped with types

--- week-16/day-01/movie-selector/script.ts
+++ week-16/day-01/movie-selector/script.ts
@@ -1,14 +1,26 @@
 const movies: NodeListOf<Element> = document.querySelectorAll('option.movie');
 const genreSelector: HTMLSelectElement = document.querySelector('#genre');
+const filmSelector: HTMLSelectElement = document.querySelector('#movies');
 
 genreSelector.addEventListener('change', (e) => {
   movies.forEach((value) => {
     value.setAttribute('style', 'display: none');
   });
-  const genreChosen = e.target.value;
+  choseGenre(e.target);
+});
+
+function choseGenre(target) {
+  const genreChosen = target.value;
   for (let i: number = 0; i < movies.length; i++) {
     if (movies[i].classList.contains(genreChosen)) {
       movies[i].setAttribute('style', 'display: initial ');
     }
   }
+}
+
+filmSelector.addEventListener('change', (e) => {
+  let result: HTMLElement = document.querySelector('.response');
+
+  result.setAttribute('style', 'color: rgba(131, 144, 204, 1); font-size: large; font-weight: bold');
+  result.innerText = (<HTMLInputElement>e.target).value;
 });
