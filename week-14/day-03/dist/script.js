904682d20136fde60b93d198db8e1c09452ccafc
Zoltan Berente <berentez07b@gmail.com>
2021-04-14 16:09:54 +0200

    connected frontend script with html and displayed post informations

---
+++ week-14/day-03/dist/script.js
@@ -0,0 +1,38 @@
+const container = document.querySelector('.container');
+
+function getposts() {
+  const httpRequest = new XMLHttpRequest();
+  httpRequest.onload = () => {
+    const posts = JSON.parse(httpRequest.responseText);
+    showPost(posts);
+  };
+  httpRequest.open('GET', 'http://localhost:3005/posts', true);
+  httpRequest.setRequestHeader('Content-Type', 'application/json');
+
+  httpRequest.send();
+}
+
+function showPost(posts) {
+  posts.forEach((element) => {
+    const divCon = document.createElement('div');
+    divCon.classList.add('post');
+    container.appendChild(divCon);
+
+    const title = document.createElement('h2');
+    const link = document.createElement('a');
+    const timestamp = document.createElement('p');
+
+    title.innerText = element.title;
+    link.innerText = element.url;
+    timestamp.innerText = element.timestamp;
+
+    divCon.appendChild(title);
+    divCon.appendChild(link);
+    divCon.appendChild(timestamp);
+    console.log(element);
+  });
+}
+
+window.onload = () => {
+  getposts();
+};
