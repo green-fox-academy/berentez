27cb192767791514da65dbf5e4192c0b93a5a4e7
Zoltan Berente <berentez07b@gmail.com>
2021-05-03 20:27:35 +0200

    get and post playlist added

--- week-19/foxPlayer/server.js
+++ week-19/foxPlayer/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mysql = require('mysql');
+const { isReturnStatement } = require('typescript');
 
 const app = express();
 const PORT = 3000;
@@ -22,6 +23,42 @@ conn.connect((err) => {
 app.use(express.json());
 app.use('/', express.static('client/dist'));
 
+//////////////////////////////////////////
+
+//List playlist
+app.get('/playlist', (req, res) => {
+  conn.query('SELECT * FROM foxplayer.playlist', (err, result) => {
+    if (err) {
+      res.status(500).send();
+      return;
+    }
+    res.status(200).json(result);
+  });
+});
+
+//create new playlist
+
+app.post('/playlist', (req, res) => {
+  const { title } = req.body;
+  conn.query('SELECT * FROM foxplayer.playlist p WHERE p.title = ?', [title], (err, result) => {
+    if (err) {
+      res.status(500).send('hups select has a server error');
+      return;
+    } else if (result.length !== 0) {
+      res.status(400).send('Playlist already exist');
+      return;
+    } else {
+      conn.query(`INSERT INTO foxplayer.playlist (title) VALUE (?)`, [title], (err, insertResult) => {
+        if (err) {
+          res.status(500).send('hups insert has a server error');
+          return;
+        }
+        res.status(200).send(insertResult);
+      });
+    }
+  });
+});
+//////////////////////////////////////////
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
