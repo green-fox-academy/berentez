435fa2dc2cda5d5f553dbdd39001c51ad74a8ce4
Zoltan Berente <berentez07b@gmail.com>
2021-05-06 18:48:33 +0200

    logo clickable

--- week-19/foxPlayer/client/dist/script.js
+++ week-19/foxPlayer/client/dist/script.js
@@ -1,6 +1,7 @@
 const category = document.querySelector('.category');
 const tracks = document.querySelector('.tracks');
 const songs = document.querySelectorAll('.tracks > p');
+const notification = document.querySelector('.notifications');
 
 const displayPlaylist = () => {
   return fetch('http://localhost:3000/playlist')
@@ -18,6 +19,13 @@ function writePlaylist(data) {
   });
 }
 
+const selectAllTracks = () => {
+  return fetch(`http://localhost:3000/playlist_tracks/`)
+    .then((res) => res.json())
+    .then((response) => writeTracks(response))
+    .catch((err) => writeError(err));
+};
+
 const listTracks = (id) => {
   return fetch(`http://localhost:3000/playlist_tracks/${id}`)
     .then((res) => res.json())
@@ -26,13 +34,22 @@ const listTracks = (id) => {
 };
 
 function writeTracks(data) {
-  console.log('fut');
-  // let counter = 1;
+  let counter = 1;
   data.forEach((song) => {
+    let num = document.createElement('span');
+    num.innerText = counter;
     const track = document.createElement('p');
     track.innerText = song.title;
+    tracks.appendChild(num);
     tracks.appendChild(track);
+    counter++;
   });
+}
+
+function writeError(err) {
+  const error = document.createElement('p');
+  error.innerText = err;
+  notification.appendChild(error);
 }
 
 window.onload = () => {
