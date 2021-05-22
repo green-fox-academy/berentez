ed825fcbc880801f5aa8254469172b306611a325
Zoltan Berente <berentez07b@gmail.com>
2021-05-22 17:13:43 +0200

    not so much progress

--- project-phase/week-21/brepi/brepi-app/src/BeerList.js
+++ project-phase/week-21/brepi/brepi-app/src/BeerList.js
@@ -1,33 +1,23 @@
 import { useState } from 'react';
+
 import Description from './Description';
 import Image from './Image';
 
 const BeerList = ({ beers }) => {
-  const [description, setDescription] = useState(false);
+  const [descriptionON, setDesc] = useState(null);
 
-  const handleClick = (beer, e) => {
-    {
-      description ? setDescription(false) : setDescription(true);
-    }
-
-    console.log(description);
+  const handleClick = () => {
+    console.log('fut');
   };
 
   return beers.map((beer) => (
-    <div className="beerTile" key={beer.id} onClick={(e) => handleClick(beer, e)}>
-      {description ? (
+    <div key={beer.id}>
+      {descriptionON ? (
         <Description description={`${beer.description}`} />
       ) : (
-        <Image image_url={`${beer.image_url}`} name={`${beer.name}`} />
+        <Image image_url={`${beer.image_url}`} name={`${beer.name}`} onClick={handleClick} />
       )}
     </div>
   ));
 };
-
 export default BeerList;
-
-{
-  /* <div className="beerTile">
-          <img src={`${beer.image_url}`} alt="beer" /> <h5>{beer.name}</h5>{' '}
-        </div> */
-}
--- project-phase/week-21/brepi/brepi-app/src/Image.js
+++ project-phase/week-21/brepi/brepi-app/src/Image.js
@@ -1,8 +1,11 @@
 const Image = (props) => {
-  console.log(props);
+  const handleClick = (e) => {
+    console.log(props.name);
+  };
+
   return (
     <div className="beerTile">
-      <img src={`${props.image_url}`} alt="beer" />
+      <img src={`${props.image_url}`} alt="beer" onClick={props.handleClick} />
       <h5>{props.name}</h5>
     </div>
   );
