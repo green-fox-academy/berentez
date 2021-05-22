f633b2609bf03a28628982059c3396b3d4af7cc9
Zoltan Berente <berentez07b@gmail.com>
2021-05-22 09:14:53 +0200

    learning React with ninja

--- project-phase/week-21/tutorial/netninja/dojo-blog/src/Home.js
+++ project-phase/week-21/tutorial/netninja/dojo-blog/src/Home.js
@@ -1,7 +1,15 @@
+import { useState, useEffect } from 'react';
+import BlogList from './BlogList';
+import useFetch from './useFetch';
+
 const Home = () => {
+  const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs');
+
   return (
     <div className="home">
-      <h2>Homepage</h2>
+      {error && <div>{error} </div>}
+      {isPending && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
     </div>
   );
 };
